293a907af164b58ab3eb6155db4b0267127d51e5
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2022-09-23 23:06:28 +0200

    cart-panel.component changed to typescript

--- week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/cart-panel.component.ts
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/cart-panel.component.ts
@@ -1,8 +1,10 @@
 import { article, p } from '@/framework/dom-creators';
 import { cartItemSum } from '@/shop/cart-item-sum.component';
 import { cartItem } from '@/shop/cart-item.component';
+import { Card } from '@/types/Card.type';
+import { CURRENCY } from '@/utils/currency.utils';
 
-export function cartPanel({ heading = 'Cart Items', items }) {
+export function cartPanel({ heading = 'Cart Items', items }: { heading?: string; items: Card[] }) {
   const $article = article('panel is-primary my-6 w-75 mx-auto');
   const $heading = p('panel-heading');
   $heading.textContent = heading;
@@ -12,6 +14,6 @@ export function cartPanel({ heading = 'Cart Items', items }) {
     value += item.price.value;
     $article.appendChild(cartItem(item));
   });
-  $article.append(cartItemSum({ value, currency: 'PLN' }));
+  $article.append(cartItemSum({ value, currency: CURRENCY.PLN }));
   return $article;
 }
