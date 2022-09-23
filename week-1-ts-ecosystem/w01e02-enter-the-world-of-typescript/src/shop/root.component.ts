308d705de3769510aa3ba69cb3b5283576732894
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2022-09-23 23:11:47 +0200

    root.component changed to typescript

--- week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/root.component.ts
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/root.component.ts
@@ -1,19 +1,21 @@
+import { Cards } from './../types/Card.type';
 import { div } from '@/framework/dom-creators';
 import { cartPanel } from '@/shop/cart-panel.component';
 import { hero } from '@/shop/hero.component';
+import { CURRENCY } from '@/utils/currency.utils';
 
-const items = [
+const items: Cards = [
   {
     name: 'Tomatoes',
     amount: 2,
     unit: 'kg',
-    price: { value: 20, currency: 'PLN' },
+    price: { value: 20, currency: CURRENCY.PLN },
   },
   {
     name: 'Banana',
     amount: 5,
     unit: 'kg',
-    price: { value: 36, currency: 'PLN' },
+    price: { value: 36, currency: CURRENCY.PLN },
   },
 ];
 
