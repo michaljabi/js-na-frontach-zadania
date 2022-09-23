85a917c401b3afa9e5ebb35f80c36626065c464f
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2022-09-23 23:10:03 +0200

    hero.component changed to typescript

--- week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/hero.component.ts
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/hero.component.ts
@@ -1,6 +1,7 @@
 import { div, p, section } from '@/framework/dom-creators';
+import { Hero } from '@/types/Hero.type';
 
-export function hero({ title, subTitle }) {
+export function hero({ title, subTitle }: Hero) {
   const $heroSection = section('hero is-link');
   const $heroBody = div('hero-body');
   const $title = p('title');
