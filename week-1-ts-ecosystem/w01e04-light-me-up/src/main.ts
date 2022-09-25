d824ba2f5fc0c7580d09f61a880cc1162f00b9ae
Tomasz Piątek <tomasz.piatek@sts.pl>
2022-09-25 14:16:24 +0200

    feat: turn on lights

--- week-1-ts-ecosystem/w01e04-light-me-up/src/main.ts
+++ week-1-ts-ecosystem/w01e04-light-me-up/src/main.ts
@@ -9,13 +9,59 @@
  * */
 
 class PowerSource {
-  private energySupply = 100
+    private energySupply = 100
 
-  consume(energy) {
-    this.energySupply -= energy
-  }
+    consume(energy: number) {
+        if ((this.energySupply - energy) < 0) {
+            throw new Error('I\'m out of power')
+        }
+
+        this.energySupply -= energy
+    }
+
+    availableEnergy() {
+        return this.energySupply;
+    }
 }
 
 class LightBulb {
-  protected readonly powerConsumption = 20
+    protected readonly powerConsumption = 20
+    powerSource: PowerSource;
+
+    constructor(powerSource: PowerSource) {
+        this.powerSource = powerSource;
+    }
+
+    consume() {
+        this.powerSource.consume(this.powerConsumption);
+    }
+
+    lightOn(sec: number) {
+        for (let s = 0; s < sec; s++) {
+            setTimeout(() => {
+                this.consume();
+                console.log('Light on');
+            }, 1000);
+        }
+    }
 }
+
+/* Task 1 */
+
+const powerSource = new PowerSource();
+let secondsOn = 0;
+const interval = setInterval(() => {
+    try {
+        new LightBulb(powerSource).consume();
+        new LightBulb(powerSource).consume();
+        secondsOn++;
+    } catch (e) {
+        clearInterval(interval);
+        console.log(`Żarówki świeciły ${secondsOn} sek.`);
+    }
+}, 1000);
+
+
+/* Task 2 */
+const powerSource2 = new PowerSource();
+new LightBulb(powerSource2).lightOn(6);
