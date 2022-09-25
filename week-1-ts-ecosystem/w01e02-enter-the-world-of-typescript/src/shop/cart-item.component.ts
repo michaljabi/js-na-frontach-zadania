4374f01f03cf640c4e42072e72632d829658944c
Paweł Zajewski <zajewskp@gmail.com>
2022-09-26 01:38:05 +0200

    cart-item.ts

--- week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/cart-item.component.ts
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/cart-item.component.ts
@@ -1,11 +1,25 @@
 import { div } from '../framework/dom-creators.js'
 
-export function cartItem({ name, amount, unit, price }) {
+export type Unit = 'kg' | 'g' | 'mg';
+export type Currency = 'PLN' | 'USD';
+
+export interface Price {
+  value: number,
+  currency: Currency
+}
+
+export interface Item {
+  name: string,
+  amount: number,
+  unit: Unit,
+  price: Price
+}
+export function cartItem({ name, amount, unit, price }: Item) {
   const $panelBlock = div('panel-block')
   const $name = div()
   $name.textContent = name
   const $amount = div('ml-auto')
-  $amount.textContent = amount
+  $amount.textContent = amount.toString();
   const $unit = div('tag')
   $unit.textContent = unit
   const $price = div('ml-4')
