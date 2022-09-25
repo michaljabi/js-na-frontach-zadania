09c67229ef44ec6cf2180e7bf9307bcfcedf0937
Damian <marusarz.damian@gmail.com>
2022-09-25 23:15:44 +0200

    feature(app-builder): testing Vite for building node app

--- week-1-ts-ecosystem/w01e03-object-oriented-structure/src/main.ts
+++ week-1-ts-ecosystem/w01e03-object-oriented-structure/src/main.ts
@@ -10,5 +10,17 @@
  * - po prostu: wykaż, że przygotowana logika i modele danych — działają :)
  * */
 
+import { Cart, CartItem } from './cart';
+
 // Dopisałem testy żeby nie śmiecić w głównym pliku i wypisywać console.logi :)
 // Uruchomienie testów => npm run test
+
+// Test for vite-build
+const cart = new Cart('BUY_NOW');
+const product = new CartItem("Apple", 2, 3, 'PLN', 'BUY_NOW')
+const product2 = new CartItem("Phone", 1, 900, 'PLN', 'BUY_NOW')
+
+cart.add(product);
+cart.add(product2);
+
+console.log(cart.getAll());
