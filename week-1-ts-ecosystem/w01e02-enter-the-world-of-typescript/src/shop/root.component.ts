d02fb110ef9083ad330ca741d40f133a5768c674
Michał Damięcki <michal.damiecki@codilime.com>
2022-09-26 10:02:10 +0200

    Refactored root component to TS.

---
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/root.component.ts
@@ -0,0 +1,28 @@
+import { div } from '../framework/dom-creators';
+import { CartItem } from '../types';
+import { cartPanel } from './cart-panel.component';
+import { hero } from './hero.component';
+
+const items: CartItem[] = [
+  {
+    name: 'Tomatoes',
+    amount: 2,
+    unit: 'kg',
+    price: { value: 20, currency: 'PLN' }
+  },
+  {
+    name: 'Banana',
+    amount: 5,
+    unit: 'kg',
+    price: { value: 36, currency: 'PLN' }
+  }
+];
+
+export function root() {
+  const $hero = hero({ title: 'Shopping App', subTitle: 'buy and sell' });
+  const $container = div('container');
+
+  $container.append($hero, cartPanel({ items }));
+
+  return $container;
+}
