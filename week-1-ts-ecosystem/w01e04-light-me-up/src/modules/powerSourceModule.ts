d293bbe8dae3f51b6f3ae92229ae01c19e1c9606
kubaGalinski <kuba@inuits.it>
2022-09-28 18:34:05 +0200

    initial commit on branch 4

--- week-1-ts-ecosystem/w01e04-light-me-up/src/modules/powerSourceModule.ts
+++ week-1-ts-ecosystem/w01e04-light-me-up/src/modules/powerSourceModule.ts
@@ -1,6 +1,6 @@
 export class PowerSource {
   private energySupply: number = 100;
-
+  
   consume(energy: number) {
     if (this.energySupply <= 0)
       throw new Error("Power source is depleted to zero");
