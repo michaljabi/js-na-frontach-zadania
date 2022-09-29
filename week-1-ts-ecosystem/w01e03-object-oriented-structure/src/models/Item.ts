8c35a6c0f010ffc52e190f5b018baad10e7d4122
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2022-09-29 11:26:35 +0200

    Item class created

--- week-1-ts-ecosystem/w01e03-object-oriented-structure/src/models/Item.ts
+++ week-1-ts-ecosystem/w01e03-object-oriented-structure/src/models/Item.ts
@@ -1,3 +1,13 @@
 export abstract class Item {
-  constructor() {}
+  readonly id: string;
+  abstract value?: number;
+  abstract currency?: string;
+  name: string;
+  amount: number;
+  constructor(name: string, amount: number) {
+    this.id = Math.round(Math.random() * Math.pow(10, 10)).toString(16);
+    this.name = name;
+    this.amount = amount;
+  }
+  abstract print(): void;
 }
