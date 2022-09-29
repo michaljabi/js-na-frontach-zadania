07205fd5777a505857f7a0039bbefd98a6077742
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2022-09-29 12:03:28 +0200

    AuctionItem class created

--- week-1-ts-ecosystem/w01e03-object-oriented-structure/src/models/AuctionItem.ts
+++ week-1-ts-ecosystem/w01e03-object-oriented-structure/src/models/AuctionItem.ts
@@ -1,3 +1,19 @@
-export class AuctionItem {
-  constructor() {}
+import { Item } from "./Item";
+
+export class AuctionItem extends Item {
+  time: number;
+  value: number;
+  currency: string;
+  constructor(name: string, amount: number, time: number, value: number, currency: string = "PLN") {
+    super(name, amount);
+    this.time = time;
+    this.value = value;
+    this.currency = currency;
+  }
+  getTimeEndAuction() {
+    return new Date(this.time);
+  }
+  print() {
+    console.log(`nazwa: ${this.name}, cena: ${this.value}, ilość: ${this.amount}, wartość: ${this.value * this.amount}`);
+  }
 }
