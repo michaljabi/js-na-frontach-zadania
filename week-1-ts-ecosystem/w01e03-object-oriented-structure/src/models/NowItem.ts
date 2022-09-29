b66f4f3b7e390c000465e7022a9475a4a14b90c2
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2022-09-29 11:31:49 +0200

    NowItem class created

--- week-1-ts-ecosystem/w01e03-object-oriented-structure/src/models/NowItem.ts
+++ week-1-ts-ecosystem/w01e03-object-oriented-structure/src/models/NowItem.ts
@@ -1,3 +1,16 @@
-export class NowItem {
-  constructor() {}
+import { Item } from "./Item";
+
+export class NowItem extends Item {
+  value: number;
+  currency: string;
+  fastShopping: boolean;
+  constructor(name: string, amount: number, value: number, currency: string = "PLN") {
+    super(name, amount);
+    this.value = value;
+    this.currency = currency;
+    this.fastShopping = true;
+  }
+  print(): void {
+    console.log(`nazwa: ${this.name}, cena: ${this.value}, ilość: ${this.amount}, wartość: ${this.value * this.amount}`);
+  }
 }
