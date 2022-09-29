a0dd98d013791b0b6c7091bffb76eba3b7f47ffd
marckraw <marckraw@icloud.com>
2022-09-29 18:39:29 +0200

    Add types to dom-creators

---
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/framework/dom-creators.ts
@@ -0,0 +1,12 @@
+function createElement(tagName: keyof HTMLElementTagNameMap = 'div', className: string = '') {
+  const element = document.createElement(tagName)
+  if (className.length) {
+    element.className = className
+  }
+  return element
+}
+
+export const p = (className: string = '') => createElement('p', className)
+export const div = (className: string = '') => createElement('div', className)
+export const section = (className: string = '') => createElement('section', className)
+export const article = (className : string = '') => createElement('article', className)
