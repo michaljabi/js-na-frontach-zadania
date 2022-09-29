cd96360e511483c5ab6b93c64c1158b6f392d8aa
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2022-09-29 13:24:21 +0200

    Show cart work, tsconfig added to project

--- week-1-ts-ecosystem/w01e03-object-oriented-structure/src/main.ts
+++ week-1-ts-ecosystem/w01e03-object-oriented-structure/src/main.ts
@@ -1,11 +1,74 @@
-/**
- * To tutaj mamy "START" programu.
- *
- * Przygotuj tutaj kawałek kodu potwierdzający poprawność działania koszyka.
- *
- * Np. Utwórz 3 różne koszyki — dodaj do nich różne produkty.
- * Potem wyświetl te produkty.
- * Wykaż, że koszyki mają różne produkty — inną ich ilość etc.
- * Przygotuj koszyki dla każdego rodzaju produktów.
- * - po prostu: wykaż, że przygotowana logika i modele danych — działają :)
- * */
+import { Cart } from "./controllers/Cart";
+import { AuctionItem } from "./models/AuctionItem";
+import { FreeItem } from "./models/FreeItem";
+import { NowItem } from "./models/NowItem";
+
+const nowItemCart = new Cart<NowItem>();
+
+const nowItem = new NowItem("Auto", 1, 230000);
+
+const freeItemCart = new Cart<FreeItem>();
+
+const freeItem1 = new FreeItem("Darmowa Książka 1", 1);
+const freeItem2 = new FreeItem("Darmowa Książka 2", 6);
+const freeItem3 = new FreeItem("Darmowa Książka 3", 9);
+
+const auctionItemCart = new Cart<AuctionItem>();
+
+const auctionItem1 = new AuctionItem("Książka", 1, Date.now() + 10000000, 54);
+const auctionItem2 = new AuctionItem("Puszki rybne", 1030, Date.now() + 100000000, 5.51);
+const auctionItem3 = new AuctionItem("Samolot", 1, Date.now() + 1000000000, 10000000);
+const auctionItem4 = new AuctionItem("Książka 4", 1, Date.now() + 10000000, 54);
+const auctionItem5 = new AuctionItem("Książka 5", 1, Date.now() + 10000000, 54);
+const auctionItem6 = new AuctionItem("Książka 6", 1, Date.now() + 10000000, 54);
+const auctionItem7 = new AuctionItem("Książka 7", 1, Date.now() + 10000000, 54);
+
+//--- Nie można dodać do koszyka normalnego, bo nie spełnia wymagań
+//nowItemCart.add(freeItem);
+//nowItemCart.add(auctionItem);
+//---
+nowItemCart.add(nowItem);
+
+//--- Nie można dodać do koszyka darmowych, bo nie spełnia wymagań
+//freeItemCart.add(nowItem);
+//freeItemCart.add(auctionItem);
+//---
+freeItemCart.add(freeItem1);
+freeItemCart.add(freeItem2);
+freeItemCart.add(freeItem3);
+
+//--- Nie można dodać do koszyka aukcji, bo nie spełnia wymagań
+//auctionItemCart.add(nowItem);
+//auctionItemCart.add(freeItem);
+//---
+auctionItemCart.add(auctionItem1);
+auctionItemCart.add(auctionItem2);
+auctionItemCart.add(auctionItem3);
+auctionItemCart.add(auctionItem4);
+auctionItemCart.add(auctionItem5);
+auctionItemCart.add(auctionItem6);
+auctionItemCart.add(auctionItem7);
+
+console.log("\x1b[35m--- Informacje o koszyku z aukcjami ---\x1b[0m\n");
+auctionItemCart.print();
+console.log("\n");
+
+console.log("\x1b[35m--- Zmiana ceny produktu o id:", auctionItem5.id, "i nazwie '", auctionItem5.name, "' ---\x1b[0m\n");
+auctionItemCart.updateById(
+  auctionItem5.id,
+  new AuctionItem(
+    auctionItemCart.findById(auctionItem5.id)!.name,
+    auctionItemCart.findById(auctionItem5.id)!.amount,
+    auctionItemCart.findById(auctionItem5.id)!.time,
+    14
+  )
+);
+auctionItemCart.print();
+console.log("\n");
+
+console.log("\x1b[35m--- Usunięcie produktu o id:", auctionItem5.id, "i nazwie '", auctionItem5.name, "' ---\x1b[0m\n");
+auctionItemCart.removeById(auctionItem5.id);
+auctionItemCart.print();
+console.log(
+  "\n\n---------------------------------------------------------------\n------------------------- KONIEC ------------------------------\n---------------------------------------------------------------\n\n"
+);
