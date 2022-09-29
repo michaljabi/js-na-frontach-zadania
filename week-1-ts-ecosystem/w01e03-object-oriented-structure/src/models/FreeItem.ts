31e17efd91098ee4374fc5d8ffaea2312c2fa6cc
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2022-09-29 12:01:46 +0200

    FreeItem class created

--- week-1-ts-ecosystem/w01e03-object-oriented-structure/src/models/FreeItem.ts
+++ week-1-ts-ecosystem/w01e03-object-oriented-structure/src/models/FreeItem.ts
@@ -1,3 +1,12 @@
-export class FreeItem {
-  constructor() {}
+import { Item } from "./Item";
+
+export class FreeItem extends Item {
+  value: undefined;
+  currency: undefined;
+  constructor(name: string, amount: number) {
+    super(name, amount);
+  }
+  print(): void {
+    console.log(`nazwa: ${this.name}, ilość: ${this.amount}`);
+  }
 }
