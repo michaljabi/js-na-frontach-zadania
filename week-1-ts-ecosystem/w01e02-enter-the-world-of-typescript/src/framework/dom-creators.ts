b0140e8622e15afa8a9f54d8ec1a9e578081bfa9
Piotr Żurek <kontakt@zurekpiotr.pl>
2022-10-01 17:57:43 +0200

    Refactor dom-creators.ts

--- week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/framework/dom-creators.ts
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/framework/dom-creators.ts
@@ -1,4 +1,6 @@
-function createElement(tagName = 'div', className = '') {
+type HtmlTagName = 'p' | 'div' | 'section' | 'article';
+
+function createElement<TagName extends  HtmlTagName>(tagName: TagName, className: string = ''){
   const element = document.createElement(tagName)
   if (className.length) {
     element.className = className
@@ -6,7 +8,7 @@ function createElement(tagName = 'div', className = '') {
   return element
 }
 
-export const p = (className = '') => createElement('p', className)
-export const div = (className = '') => createElement('div', className)
-export const section = (className = '') => createElement('section', className)
-export const article = (className = '') => createElement('article', className)
+export const p = (className: string = ""): HTMLParagraphElement => createElement("p", className);
+export const div = (className: string = ""): HTMLDivElement => createElement("div", className);
+export const section = (className: string = ""): HTMLElement => createElement("section", className);
+export const article = (className: string = ""): HTMLElement => createElement("article", className);
