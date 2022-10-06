b1edfd9931a12c157882e53013d920075172f27e
DragoBJJ <j.pawelski.it@gmail.com>
2022-10-06 18:31:25 +0200

    delete unused import

--- week-2-wide-perspective/w02e03-pagination-problem/src/middleware.js
+++ week-2-wide-perspective/w02e03-pagination-problem/src/middleware.js
@@ -1,5 +1,3 @@
-import {usersRepository} from "./users/users.repository.js";
-
 export const paginationMiddleware = function (req, res, next) {
     const {skip, limit} = req.query
     let computedSkip = 0
