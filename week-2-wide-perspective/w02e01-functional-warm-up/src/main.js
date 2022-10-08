f35b142d9ad938df56b4d1c0370ed91cb3f2cbbe
Damian <marusarz.damian@gmail.com>
2022-10-08 21:44:55 +0200

    Review me

--- week-2-wide-perspective/w02e01-functional-warm-up/src/main.js
+++ week-2-wide-perspective/w02e01-functional-warm-up/src/main.js
@@ -3,23 +3,46 @@ import { fruitsData } from './data-store/fruits.js'
 import { randomAlbumData } from './data-store/music-album.js'
 
 // Do wyliczenia danych poniżej użyj faktycznego źródła: randomAlbumData
+
+function getTheShortestTrack(trackList) {
+    const sortedTracks = trackList
+    .map(track =>  Object.assign(track, { duration: +track.duration }))
+    .sort((a,b) => a.duration - b.duration);
+    return sortedTracks[0];
+}
+
 console.log(`-- Album Info -- 
-Name: ${'The name'}
-Artist: ${'Adele'}
-Number of tracks: ${0}
-Total album duration: ${0}
-Shortest track: "${'Rumour Has It'}"
+Name: ${randomAlbumData.title}
+Artist: ${randomAlbumData.artist.name}
+Number of tracks: ${randomAlbumData.tracks.length}
+Total album duration: ${randomAlbumData.tracks.reduce((a,b) => a + (+b.duration), 0 )}
+Shortest track: "${getTheShortestTrack(randomAlbumData.tracks).title}"
 `)
 
 // Do wyliczenia danych poniżej użyj faktycznego źródła: fruitsData
+
+function getUniqValues(list) {
+    return Array.from(new Set(list))
+}
+
+function getUniqValuesStartWithLetter(list, letter) {
+    const uniqList = getUniqValues(list);
+    return uniqList.filter(element => element.startsWith(letter))
+}
+
+const lowestCaloriesFruit = fruitsData.sort((a, b) => a.nutritions.calories - b.nutritions.calories)[0]
+
 console.log(`-- Fruits Info -- 
-All fruit families: ${['Rosaceae', 'Moraceae']}
-All fruit starts with "g": ${['Guava', 'GreenApple']}
-Lowest calories: ${'Cherry, 50 calories'}
+All fruit families: ${getUniqValues(fruitsData.map(fruit => fruit.family))}
+All fruit starts with "g": ${getUniqValuesStartWithLetter(fruitsData.map(fruit => fruit.name), "G")}
+Lowest calories: ${lowestCaloriesFruit.name}, ${lowestCaloriesFruit.nutritions.calories} calories 
 `)
 
 // Do wyliczenia danych poniżej użyj faktycznego źródła: usersData
+
+const shortestWebsiteName = usersData.sort((a, b) => a.website.length - b.website.length)[0].website;
+
 console.log(`-- Users Info -- 
-All user names: ${['Leanne', 'Ervin', 'Clementine']}
-User shortest website name: ${'conrad.com'}
+All user names: ${usersData.map(user => user.name.split(' ')[0])}
+User shortest website name: ${shortestWebsiteName}
 `)
