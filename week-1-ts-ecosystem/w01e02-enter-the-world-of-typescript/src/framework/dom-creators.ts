d9968de63d2b2011cc871f5743ba99793e6adbf3
MarcinParda <marcin98parda@gmail.com>
2022-10-20 13:55:44 +0200

    migrate dom-creators to ts

---
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/framework/dom-creators.ts
@@ -0,0 +1,14 @@
+type TagType = 'div' | 'p' | 'section' | 'article';
+
+function createElement<Tag extends TagType>(tagName: Tag, className = '') {
+  const element = document.createElement(tagName);
+  if (className.length) {
+    element.className = className;
+  }
+  return element;
+}
+
+export const p = (className = '') => createElement('p', className);
+export const div = (className = '') => createElement('div', className);
+export const section = (className = '') => createElement('section', className);
+export const article = (className = '') => createElement('article', className);
