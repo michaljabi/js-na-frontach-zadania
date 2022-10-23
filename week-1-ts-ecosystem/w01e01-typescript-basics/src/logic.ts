9c6899c2e9622ad0b5acf862998babc0af474b0e
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-10-23 11:34:31 +0100

    zmien any na Function

--- week-1-ts-ecosystem/w01e01-typescript-basics/src/logic.ts
+++ week-1-ts-ecosystem/w01e01-typescript-basics/src/logic.ts
@@ -4,7 +4,7 @@ function hasAddress(user: User): boolean {
     return Boolean(user.address)
   }
   
-function hasGivenAge(requiredAge: number): any {
+function hasGivenAge(requiredAge: number): Function {
     return (user: User): boolean => user.age >= requiredAge
 }
 
