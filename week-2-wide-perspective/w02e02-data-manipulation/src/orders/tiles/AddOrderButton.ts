f45160bc3aac7e32b4c944382cacacf0f33768f2
adam <adam.jacewicz4@gmail.com>
2022-11-07 13:14:16 +0100

    - use rxjs event listening approach

--- week-2-wide-perspective/w02e02-data-manipulation/src/orders/tiles/AddOrderButton.ts
+++ week-2-wide-perspective/w02e02-data-manipulation/src/orders/tiles/AddOrderButton.ts
@@ -1,11 +1,12 @@
 import { Orders } from "../../model/orders";
+import { fromEvent } from "rxjs";
 
 const button = document.querySelector('[data-button="add-order"]');
 
 export function initAddOrderButton(orders: Orders) {
   if (button) {
-    button.addEventListener("click",  () => {
-      orders.addOrder(22222);
+    fromEvent(button, "click").subscribe(() => {
+      orders.addOrder(555.5);
     });
   }
 }
